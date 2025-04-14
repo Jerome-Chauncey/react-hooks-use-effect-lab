5a8c9d1b2d0e84aba56a33159cb767776112c5cd
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-04-14 19:26:21 +0300

    creates a countdown timer so that the user has to answer to each question within 10 seconds

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() =>{
+    if(timeRemaining > 0){
+      const timeoutId = setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+
+      return () => clearTimeout(timeoutId);
+    }else{
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]) 
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -30,3 +42,5 @@ function Question({ question, onAnswered }) {
 }
 
 export default Question;
+
+
